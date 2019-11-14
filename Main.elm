d2557450e6b2ec038e64fe6908d43c2ed00d7e7e
Pankaj Sonawane <pankajso@gmail.com>
2019-11-14 15:05:29 +0530

    Basic structure created

--- Main.elm
+++ Main.elm
@@ -14,12 +14,30 @@ import Url.Parser.Query as Query
 
 
 type alias Model =
-    {}
+    { io : String
+    , prev : String
+    , operation : Operation
+    }
 
 
 init : Model
 init =
-    {}
+    { io = "0"
+    , prev = "0"
+    , operation = None
+    }
+
+
+
+-- % + - *
+
+
+type Operation
+    = Div
+    | Mult
+    | Add
+    | Subst
+    | None
 
 
 
@@ -43,11 +61,45 @@ update message model =
 
 
 ---- VIEW ----
+-- button [onClick msg, style btnStyle] [text string]
 
 
 view : Model -> Html Msg
 view model =
-    Html.text "xx"
+    Html.div []
+        [ Html.div []
+            [ Html.div [] [ Html.text "0" ]
+            , Html.div []
+                [ Html.button [] [ Html.text "C" ]
+                , Html.button [] [ Html.text "-" ]
+                , Html.button [] [ Html.text "%" ]
+                , Html.button [] [ Html.text "/" ]
+                ]
+            , Html.div []
+                [ Html.button [] [ Html.text "7" ]
+                , Html.button [] [ Html.text "8" ]
+                , Html.button [] [ Html.text "9" ]
+                , Html.button [] [ Html.text "X" ]
+                ]
+            , Html.div []
+                [ Html.button [] [ Html.text "4" ]
+                , Html.button [] [ Html.text "5" ]
+                , Html.button [] [ Html.text "6" ]
+                , Html.button [] [ Html.text "-" ]
+                ]
+            , Html.div []
+                [ Html.button [] [ Html.text "1" ]
+                , Html.button [] [ Html.text "2" ]
+                , Html.button [] [ Html.text "3" ]
+                , Html.button [] [ Html.text "+" ]
+                ]
+            , Html.div []
+                [ Html.button [] [ Html.text "0" ]
+                , Html.button [] [ Html.text "." ]
+                , Html.button [] [ Html.text "=" ]
+                ]
+            ]
+        ]
 
 
 
